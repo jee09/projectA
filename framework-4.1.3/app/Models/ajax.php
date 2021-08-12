380f94a5ae673bafefdb9aba4889281febf69b16
saha <jungwonji1960@gmail.com>
2021-08-13 01:45:12 +0900

    [0812][#로그인] 로그인 페이지 및 통신 개발

---
+++ framework-4.1.3/app/Models/ajax.php
@@ -0,0 +1,19 @@
+
+<?php
+echo '<script>';
+echo 'alert("ajax.php");';
+echo '</script>';
+
+$id = 'saha';
+$pw = '랑이4946';
+
+function dfsd()
+{
+    $formId = $_GET['form_data']['userId'];
+    $formPw = $_GET['form_data']['userPw'];
+}
+
+if ($id == $_GET ['form_data']['id'])
+{
+    //return (result=>'sussce');
+}
--- framework-4.1.3/app/login.php
+++ framework-4.1.3/app/login.php
@@ -3,45 +3,36 @@
 <title>jQuery 로그인</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $("#login").click(function() {
-            let action = $("#form1").attr('action');
-            let form_data = {
-                user_id: $("#user_id").val(),
-                user_pw: $("#user_pw").val(),
-                is_ajax: 1
+    function login_ajax(value)
+    {
+        var form_data =
+            {
+                id: $("#userId").val(),
+                pw: $("#userPw").val()
             };
-            $.ajax({
-                type: "POST",
-                url: action,
-                data: form_data,
-                success: function(response) {
-                    if(response == 'success') {
-                        $("#message").html("<p style='color:green;font-weight:bold'>로그인 성공!</p>");
-                        $("#form1").slideUp('slow');
-                    }
-                    else {
-                        $("#message").html("<p style='color:red'>아이디 또는 비밀번호가 잘못되었습니다.</p>");
-                    }
-                }
-            });
-            return false;
+        console.log(form_data);
+
+        $.ajax({
+            type: "GET",
+            url: "/Models/ajax.php",
+            dataType: "json",
+            data: form_data,
+            success: function(response)
+            {
+                alert("code:"+response.status+"\n");
+            },
+            error:function(request,status,error)
+            {
+                alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            }
         });
-    });
+        console.log(1);
+    }
 </script>
-<body>
-<form id="form1" name="form1" action="login_ok.php" method="post">
-    <table>
-        <tr>
-            <td>아이디</td>
-            <td><input type='text' id='user_id' name='user_id' tabindex='1'/></td>
-            <td rowspan='2'><input type='button' id='login' tabindex='3' value='로그인' style='height:50px'/></td>
-        </tr>
-        <tr>
-            <td>비밀번호</td>
-            <td><input type='password' id='user_pw' name='user_pw' tabindex='2'/></td>
-        </tr>
-    </table>
+
+<form onsubmit="return login_ajax(this)">
+    <input type="text" id="userId" placeholder="ID">아이디<br>
+    <input type="password" id="userPw" placeholder="PW">비밀번호<br>
+    <input type="submit" value="로그인">
 </form>
-<div id="message"></div>
-</body>
+<br><br><br>
